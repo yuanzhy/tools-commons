412a26ba57ac852584fb42b83ff6fd8d00511bf1
yuanzhy <529164645@qq.com>
2021-07-03 21:19:10 +0800

    first commit

---
+++ src/main/java/com/yuanzhy/tools/commons/io/IOUtilsDemo.java
@@ -0,0 +1,7 @@
+package com.yuanzhy.tools.commons.io;
+
+public class IOUtilsDemo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/yuanzhy/tools/commons/lang/StringUtilsDemo.java
@@ -0,0 +1,10 @@
+package com.yuanzhy.tools.commons.lang;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringUtilsDemo {
+
+    public static void main(String[] args) {
+        StringUtils.isEmpty("");
+    }
+}
